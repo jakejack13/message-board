c7028bb89616ca49a8da59117ddb6d1fdcd8fd9e
Jacob Kerr <jck268@cornell.edu>
2025-04-24 18:40:16 -0400

    Even more prod readiness

--- atomhacks/settings.py
+++ atomhacks/settings.py
@@ -27,8 +27,9 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['.message-board.net', 'localhost', '127.0.0.1']
 
+CSRF_TRUSTED_ORIGINS = ['https://message-board.net']
 
 # Application definition
 
