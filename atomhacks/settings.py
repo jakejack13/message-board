1295df6ca0baf669cd88584032c4ad2c37433921
Jacob Kerr <jck268@cornell.edu>
2025-04-24 17:51:12 -0400

    Get app production ready

--- atomhacks/settings.py
+++ atomhacks/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 from typing import Any
 
@@ -21,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-)ip-n=(tlf4@hi6bg^q41tq3ltk-&-nz90*=r#o7*@yo7un&z%"
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -39,7 +40,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "markdownify.apps.MarkdownifyConfig",
 ]
 
 MIDDLEWARE = [
