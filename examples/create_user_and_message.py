c499d76a82531f9b18647653fee13218c0866ecb
Jacob Kerr <jck268@cornell.edu>
2025-04-25 23:37:52 -0400

    Update examples

--- examples/create_user_and_message.py
+++ examples/create_user_and_message.py
@@ -3,12 +3,12 @@ import requests
 BASE_URL = 'https://message-board.net'
 
 # Create new user
-body = {'username': 'JacobTest', 'password': 'Test'}
+body = {'username': 'Jacob', 'password': 'Test'}
 response = requests.post(BASE_URL + '/messaging/user', json=body)
 print(response.status_code)
 
 # Create new message
-headers = {'Username': 'JacobTest', 'Password': 'Test'}
-body = {'message': 'Hi everyone!'}
+headers = {'Username': 'Jacob', 'Password': 'Test'}
+body = {'message': 'I made a message!'}
 response = requests.post(BASE_URL + '/messaging/message/create', json=body, headers=headers)
 print(response.status_code)
